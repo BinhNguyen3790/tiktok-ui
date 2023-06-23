ea1f79c8808d7341c6dad2ce9daf600ab34dafb9
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2023-06-23 10:15:08 +0700

    custome source

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -20,7 +20,7 @@ import { UploadIcon, MessageIcon } from "@/components/Icons";
 import Image from "@/components/Images";
 import Search from "../Search";
 import { Link } from "react-router-dom";
-import routesConfig from "@/config/routes";
+import config from "@/config";
 
 const cx = classNames.bind(styles);
 const MENU_ITEMS = [
@@ -225,7 +225,7 @@ function Header() {
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <div className={cx("logo")}>
-          <Link to={routesConfig.home} className={cx("logo-link")}>
+          <Link to={config.routes.home} className={cx("logo-link")}>
             <img src={images.logo} alt="Tiktok" />
           </Link>
         </div>
