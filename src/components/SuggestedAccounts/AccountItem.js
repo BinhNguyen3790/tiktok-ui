6e3b94a5f511758548a0fbb3883f25b0249d9a46
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2023-07-06 09:47:03 +0700

    update ode

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,24 +1,41 @@
 import PropTypes from "prop-types";
 import classNames from "classnames/bind";
+import Tippy from "@tippyjs/react/headless";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { Wrapper as PopperWrapper } from "@/components/Popper";
 
 import images from "@/assets/images";
 import styles from "./SuggestedAccounts.module.scss";
+import AccountPreview from "./AccountPreview/AccountPreview";
 
 const cx = classNames.bind(styles);
 
+const renderPreview = (props) => {
+  return (
+    <div tabIndex="-1" {...props}>
+      <PopperWrapper>
+        <AccountPreview />
+      </PopperWrapper>
+    </div>
+  );
+};
+
 function AccountItem() {
   return (
-    <div className={cx("account-item")}>
-      <img className={cx("avatar")} src={images.userImg} alt="" />
-      <div className={cx("item-info")}>
-        <p className={cx("nickname")}>
-          <strong>Binhdevfe</strong>
-          <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
-        </p>
-        <p className={cx("name")}>Binhdev</p>
-      </div>
+    <div>
+      <Tippy interactive delay={[800, 0]} placement="bottom" render={renderPreview}>
+        <div className={cx("account-item")}>
+          <img className={cx("avatar")} src={images.userImg} alt="" />
+          <div className={cx("item-info")}>
+            <p className={cx("nickname")}>
+              <strong>Binhdevfe</strong>
+              <FontAwesomeIcon className={cx("check")} icon={faCheckCircle} />
+            </p>
+            <p className={cx("name")}>Binhdev</p>
+          </div>
+        </div>
+      </Tippy>
     </div>
   );
 }
